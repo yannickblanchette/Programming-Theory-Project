7cd5cff77d369390ec6536c45da631e73c5c99da
yannickblanchette <72986036+yannickblanchette@users.noreply.github.com>
2025-07-03 11:37:52 -0400

    Implemented Update best score and score text
    Implemented Update best score and score text

--- Assets/Scripts/GameScreenManager.cs
+++ Assets/Scripts/GameScreenManager.cs
@@ -1,16 +1,59 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameScreenManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI bestScoreText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private int playerScore;
+
+    private const string bestScoreTextBase = "Best Score: {name}: {score}";
+    private const string scoreTextBase = "Score: {name}: {score}";
+
+
+    private void Awake()
+    {
+        InitiliazeLocalVariables();
+        UpdateScoreText();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpdateBestScoreText();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void InitiliazeLocalVariables()
+    {
+        playerScore = 0;
+    }
+
+
+    private void UpdateBestScoreText()
+    {
+        bestScoreText.text = $"Best Score: Yannick: 9";
+    }
+
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = $"Score: {GameManager.instance.playerName}: {playerScore}" ;
+    }
+
+
+    public void IncrementScore(int score)
+    {
+        playerScore += score;
+        UpdateScoreText();
     }
 }
