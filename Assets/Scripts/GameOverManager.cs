5b8fa98a249c180bd228e36c4a5714c0f8fc0b9a
yannickblanchette <72986036+yannickblanchette@users.noreply.github.com>
2025-07-04 08:59:19 -0400

    Implement DisplayBestScoreList
    Implemented DisplayBestScoreList to show the best 5 best scores

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,57 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager instance {  get; private set; }
+    
+    [SerializeField] private TextMeshProUGUI bestScoreListText;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DisplayBestScoreList();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    // ABSTRACTION
+    private void DisplayBestScoreList()
+    {
+        //First display the title of the table
+        string toDisplay = "Best Scores";
+        
+        //Display each best score in the list if a best score exists
+        for (int i = 0; i < BestScoreManager.bestScoreArrayLength; i++)
+        {
+            toDisplay += Environment.NewLine;
+
+            //Check if the entry is suitable to be displayed
+            BestScoreManager.BestScoreEntry entry = BestScoreManager.instance.bestScoreArray[i];
+            if (!string.IsNullOrEmpty(entry.name) && entry.score >= 0)
+            {
+                toDisplay += $"{i}: {entry.name} : {entry.score}";
+            }
+        }
+
+        bestScoreListText.text = toDisplay;
+    }
+
+}
